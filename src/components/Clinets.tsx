1cb797469879160bf663b5c5435c0c916806d47c
Ramesh6060 <chetbahadur611@gmail.com>
2026-03-10 19:51:35 +0545

    Initial commit

---
+++ src/components/Clinets.tsx
@@ -0,0 +1,12 @@
+import { div } from 'motion/react-client';
+import React from 'react';
+
+const Clients = () => {
+    return(
+        <div>
+            Clients
+        </div>
+    )
+}
+
+export default Clients
